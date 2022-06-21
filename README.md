3918eddbcec8b00bf8fa79accec98ed6ca3a3bb4
zhangpk <17666210109@163.com>
2022-06-21 10:23:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+### 教程
+https://blog.csdn.net/weixin_37551036/article/details/115415358
+# glados-checkin
+![glados-checkin](https://github.com/hbstarjason/glados-checkin/workflows/glados-checkin/badge.svg)
+
+#### 注册地址：
+
+1、打开 https://github.com/glados-network/GLaDOS ，找到[<u>***Register***</u>]，打开链接，填写邮箱进行登录。
+
+2、输入激活码`LC4CI-CBO6S-RXV13-LT7ND`，进行激活，获得3天试用。
+
+3、每天手动进行checkin一次，能增加一天。
+
+
+
+#### 脚本功能：
+
+1、通过Github Action自动定时运行[checkin.py](https://github.com/hbstarjason/glados-checkin/blob/master/checkin.py)脚本。
+
+2、通过cookies自动登录（[https://glados.rocks/console/checkin](https://glados.rocks/console/checkin))，脚本会自动进行checkin。
+
+3、然后通过“Server酱”（[https://sct.ftqq.com/](https://sct.ftqq.com/))，自动发通知到微信上。
+
+
+
+#### 食用姿势：
+
+1. 先“Fork”本仓库。（不需要修改任何文件！）
+
+2. 注册GLaDOS，方法见上。
+
+3. 登录GLaDOS后获取cookies。（简单获取方法：浏览器快捷键F12，打开调试窗口，点击“network”获取）
+
+4. 在自己的仓库“Settings”里创建3个“Secrets”，分别是：（不开启通知，只需要创建一个COOKIE即可）
+
+   - COOKIE（**必填**）
+   - SERVE（server酱开关，默认是off，填on的话，会同时开启cookie失效通知和签到成功通知）
+   - SCKEY（填写server酱sckey，不开启server酱则不用填）
+
+5. 以上设置完毕后，每天零点会自动触发，并会执行自动checkin，如果开启server酱，会自动发通知到微信上。
+
+6. **如果以上都不会的话，注册GLaDOS后，每天勤奋点记得登录后手动进行checkin即可。**
+
+   [*<u>如果是Edu邮箱，可免费升级为360天。</u>*]
---
+++ checkin.py
@@ -0,0 +1,51 @@
+import requests,json,os
+
+# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
+sever = os.environ["SERVE"]
+
+# 填入glados账号对应cookie
+cookie = os.environ["COOKIE"]
+
+# 企业微信机器人地址
+rot_url=os.environ['ROTURL']
+
+def start():
+    url= "https://glados.rocks/api/user/checkin"
+    url2= "https://glados.rocks/api/user/status"
+    referer = 'https://glados.rocks/console/checkin'
+    #checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
+    #state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
+    origin = "https://glados.rocks"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+    payload={ 
+        # 'token': 'glados_network'
+        'token': 'glados.network'
+    }
+    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
+    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+    print(checkin.text,'\n')
+    print(state.text)
+
+
+    if sever == 'on':
+        robot_msg=None
+        if 'message' in checkin.text:
+            mess = checkin.json()['message']
+            time = state.json()['data']['leftDays']
+            time = time.split('.')[0]
+            print(time)
+            robot_msg = mess+'，you have '+time+' days left'
+        else:
+            robot_msg='cookie过期'
+        headers = {"Content-Type": "application/json;charset=UTF-8"}
+        text_data = {"content" : robot_msg}
+        req_data = {"msgtype" : "text", "text" : text_data}
+        res = requests.request('post', rot_url, data=json.dumps(req_data), headers=headers)
+
+def main_handler(event, context):
+  return start()
+
+if __name__ == '__main__':
+    start()
+
+    
